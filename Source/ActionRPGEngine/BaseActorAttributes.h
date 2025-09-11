27b6f4fafb53bade7ea51c7d8e6a30da5d36328f
Robert Scalice <r.scalice@live.com>
2025-09-10 23:07:00 -0400

    adding C++ for attributes

---
+++ Source/ActionRPGEngine/BaseActorAttributes.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "BaseActorAttributes.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+
+/**
+ *
+ */
+UCLASS()
+class ACTIONRPGENGINE_API UBaseActorAttributes : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UBaseActorAttributes, Health);
+};
